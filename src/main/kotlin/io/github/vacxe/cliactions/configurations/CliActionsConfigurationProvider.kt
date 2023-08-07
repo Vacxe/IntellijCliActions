fa48a51979209f63e7b6c24eaf57e90386184d27
Konstantin Aksenov <aksenov.kostya@gmail.com>
2023-08-07 15:35:11 +1000

    fix large project search

--- src/main/kotlin/io/github/vacxe/cliactions/configurations/CliActionsConfigurationProvider.kt
+++ src/main/kotlin/io/github/vacxe/cliactions/configurations/CliActionsConfigurationProvider.kt
@@ -3,18 +3,15 @@ package io.github.vacxe.cliactions.configurations
 import com.intellij.openapi.project.Project
 import org.apache.commons.io.FileUtils
 import java.io.File
-import java.lang.Thread.sleep
 import kotlin.concurrent.thread
 
-class CliActionsConfigurationProvider(private val project: Project) : ConfigurationProvider{
+class CliActionsConfigurationProvider(private val project: Project) : ConfigurationProvider {
     override fun find(result: (Sequence<File>) -> Unit) {
         thread(start = true) {
-            sleep(10000)
             val projectConfigs = File(project.basePath ?: throw Exception("Project basePath cannot be found"))
                 .walk()
+                .maxDepth(2)
                 .filter { it.name.endsWith(ConfigurationFileExtension) }
-
-            FileUtils.getUserDirectory()
 
             val userConfigs = FileUtils.getUserDirectory()
                 .walk()
