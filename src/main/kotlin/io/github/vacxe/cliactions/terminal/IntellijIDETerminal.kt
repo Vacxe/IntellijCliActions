2334db6e4eb0a3fa04d083e4885d25e6406b4fc4
Konstantin Aksenov <aksenov.kostya@gmail.com>
2024-02-06 08:03:19 +1000

    reusability for tabs

--- src/main/kotlin/io/github/vacxe/cliactions/terminal/IntellijIDETerminal.kt
+++ src/main/kotlin/io/github/vacxe/cliactions/terminal/IntellijIDETerminal.kt
@@ -1,14 +1,25 @@
 package io.github.vacxe.cliactions.terminal
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindowManager
+import org.jetbrains.plugins.terminal.ShellTerminalWidget
+import org.jetbrains.plugins.terminal.TerminalToolWindowFactory
 import org.jetbrains.plugins.terminal.TerminalView
 import java.io.IOException
 
 class IntellijIDETerminal(private val project: Project) : Terminal {
     override fun run(name: String, command: String) {
         val terminalView: TerminalView = TerminalView.getInstance(project)
+        val window = ToolWindowManager.getInstance(project).getToolWindow(TerminalToolWindowFactory.TOOL_WINDOW_ID)
+        val contentManager = window?.contentManager
+
         try {
-            terminalView.createLocalShellWidget(project.basePath, name).executeCommand(command)
+            val widget = when (val content = contentManager?.findContent(name)) {
+                null -> terminalView.createLocalShellWidget(project.basePath, name)
+                else -> TerminalView.getWidgetByContent(content) as ShellTerminalWidget
+            }
+
+            widget.executeCommand(command)
         } catch (err: IOException) {
             err.printStackTrace()
         }
